56319a0c0195adb926b92f66e070d1fa4e240728
Chris Wan <wanchris1997@gmail.com>
2019-09-14 12:29:00 -0400

    Changing version

---
+++ Keep-Shopping-and-Everyone-Gets-Home/Assets/GrabAndMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GrabAndMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(Camera.main.transform.foward); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
