1061bea425467231bc347a20f12043ea21734e7c
Chris Wan <wanchris1997@gmail.com>
2019-09-14 13:43:31 -0400

    Score

---
+++ Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/AddScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddScore : MonoBehaviour
+{
+    public int score = 0; 
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Fruit")
+        {
+            collision.gameObject.SetActive(false);
+            score += 100; 
+            Debug.Log("Score increased!");
+            Debug.Log(score);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 100), score.ToString()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
---
+++ Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/GrabAndMove.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class GrabAndMove : MonoBehaviour
+{
+    GameObject grabbedObject;
+    float grabbedObjectSize; 
+
+
+    GameObject GetMouseHoverObject(float range)
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + Camera.main.transform.forward * range;
+
+        if (Physics.Linecast(position, target, out raycastHit))
+        {
+            return raycastHit.collider.gameObject;
+        }
+        else
+        {
+            return null; 
+        }
+    }
+
+    void TryGrabObject(GameObject grabObject)
+    {
+        if (grabObject == null)
+        {
+            return; 
+        }
+
+        grabbedObject = grabObject;
+        grabbedObjectSize = grabObject.GetComponent<Renderer>().bounds.size.magnitude; 
+    }
+
+    void DropObject()
+    {
+        if (grabbedObject == null)
+        {
+            return;
+        }
+        grabbedObject = null; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        { 
+            if (grabbedObject == null)
+            {
+                TryGrabObject(GetMouseHoverObject(10));
+            }
+            else
+            {
+                DropObject(); 
+            }            
+        }
+
+        //Debug.Log(grabbedObject.tag); 
+
+        if (grabbedObject != null && grabbedObject.tag == "Fruit")
+        {
+            //Debug.Log("Fruit clicked!");
+            Vector3 newPosition = gameObject.transform.position + Camera.main.transform.forward * grabbedObjectSize;
+            grabbedObject.transform.position = newPosition; 
+        }
+    }
+}
