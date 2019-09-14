ca1349f76f9abdf5c01c29ebe3d8de3f2d7c218d
Anthony Gruskovnjak <AGruskovnjak@Anthonys-MacBook-Air.local>
2019-09-14 15:04:01 -0400

    potatoes

--- Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/Movement.cs
+++ Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/Movement.cs
@@ -2,27 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour
 {
+   
   public float speed = 10.0F;
+
+
+
+
 
   // Start is called before the first frame update
   void Start(){
-    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.lockState = CursorLockMode.None;
+
   }
   // Update is called once per frame
   void Update(){
 
+    if (gameObject.GetComponent<timer>().shouldMove()){  
   	float translation = Input.GetAxis("Vertical") * speed;
   	float strafe = Input.GetAxis("Horizontal") * speed;
 	
   	translation *= Time.deltaTime;
   	strafe *= Time.deltaTime;
   	transform.Translate(strafe, 0, translation);
-	
-  	if (Input.GetKeyDown("escape")){
-  	  Cursor.lockState=CursorLockMode.None;
-  	}
-       
+  }
+    
    }
 }
--- Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/timer.cs
+++ Keep-Shopping-and-Everyone-Gets-Home/Assets/Sccripts/timer.cs
@@ -6,29 +6,49 @@ using UnityEngine;
   
 public class timer : MonoBehaviour
 {
-  [SerializeField] private Text uiText;
-  [SerializeField] private float mainTimer;
+  private string uiText;
+  private float mainTimer;
 
-  private float timer1 = 10.0f;
+  public float timer1 = 3.0f;
+  public float timer2 = 33.0f;
 
   private string state;
   private bool canCount = true;
   private bool doOnce = false;
+  bool move = false;
 
   void start(){
-      // timer1 = 30.0f;
     state = "start";
   }
-
- void Update(){
-  if (timer1 >= 0.0f && canCount){
+  void Update(){
+ 	// Start 3-sec
+  if (timer1 >= 0.0f){
     timer1 =timer1 - Time.deltaTime;
-    uiText.text = timer1.ToString("F");
-  }else if (timer1 <= 0.0f && ! doOnce){
-    canCount = false;
-    doOnce = true;
-    uiText.text = "Game Over";
-    timer1 = 0.0f;
+    timer2 = timer2 - Time.deltaTime;
+    uiText = timer1.ToString("F");
+
+  }else if (timer2 >= 0.0f){
+  	move = true;
+  	// "Main" Game
+  	timer2 = timer2 - Time.deltaTime;
+    uiText = timer2.ToString("F");
+
+  }else{
+  	// EndGame
+  	move = false;
+    uiText = "Game Over";
+    timer1 = -1.0f;
   }
  }
+
+  void OnGUI()
+  {
+    GUI.Label(new Rect(10, 100, 100, 100), uiText.ToString()); 
+  }
+
+  public bool shouldMove(){
+  	return move;
+  }
+
+ 
 }
